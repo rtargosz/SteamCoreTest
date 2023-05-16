83c5ed89619b0ac255b7ac27a8529f5940fcac12
Bent Vector Studios <rtargosz@bentvector.com>
2023-05-16 19:58:08 -0400

    Initial commit to test UGC code

---
+++ Source/SteamCoreTest/SteamCoreTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteamCoreTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEAMCORETEST_API ASteamCoreTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
